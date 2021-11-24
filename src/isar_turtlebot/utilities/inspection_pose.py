d9ad10ea35f23bfe264973f069a4809846579121
Kristoffer <knord@equinor.com>
2021-11-24 17:22:45 +0100

    Add check for inspection pose

--- src/isar_turtlebot/utilities/inspection_pose.py
+++ src/isar_turtlebot/utilities/inspection_pose.py
@@ -1,27 +1,29 @@
 import numpy as np
-from robot_interface.models.geometry.frame import Frame
-from robot_interface.models.geometry.orientation import Orientation
-from robot_interface.models.geometry.pose import Pose
-
 
 from scipy.spatial.transform import Rotation
 
+from robot_interface.models.geometry.frame import Frame
+from robot_interface.models.geometry.orientation import Orientation
+from robot_interface.models.geometry.pose import Pose
+from robot_interface.models.geometry.position import Position
 
-def get_inspection_pose(current_pose, target):
+
+def get_inspection_pose(current_pose: Pose, target: Position):
 
     direction = np.array(target.to_list()) - np.array(current_pose.position.to_list())
-
     alpha = np.arctan2(direction[0], direction[1])
-
-    rot = Rotation.from_euler("zyx", [alpha, 0, 0], degrees=False)
-    rotation = rot.as_quat()
+    rotation = Rotation.from_euler("zyx", [alpha, 0, 0], degrees=False)
+    quaternion = rotation.as_quat()
 
     orientation = Orientation(
-        x=rotation[0], y=rotation[1], z=rotation[2], w=rotation[3], frame=Frame.Robot
+        x=quaternion[0],
+        y=quaternion[1],
+        z=quaternion[2],
+        w=quaternion[3],
+        frame=Frame.Robot,
     )
 
     pose = Pose(
         position=current_pose.position, orientation=orientation, frame=Frame.Robot
     )
-
     return pose
