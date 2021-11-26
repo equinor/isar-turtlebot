0e3b3cb2a4ae98b2babcac7cdbdd71a63bcc1185
Vetle Koppergård <vetk@equinor.com>
2021-11-26 08:20:00 +0100

    Fix computing of yaw angle to target

--- src/isar_turtlebot/utilities/inspection_pose.py
+++ src/isar_turtlebot/utilities/inspection_pose.py
@@ -11,7 +11,7 @@ from robot_interface.models.geometry.position import Position
 def get_inspection_pose(current_pose: Pose, target: Position) -> Pose:
 
     direction = np.array(target.to_list()) - np.array(current_pose.position.to_list())
-    alpha = np.arctan2(direction[0], direction[1])
+    alpha = np.arctan2(direction[1], direction[0])
     rotation = Rotation.from_euler("zyx", [alpha, 0, 0], degrees=False)
     quaternion = rotation.as_quat()
 
