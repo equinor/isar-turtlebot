7ff9ad46e49ab400237f29cdb2d45096b46078ab
Sondre Olsen <sondre.olsen97@gmail.com>
2021-12-17 11:06:27 +0100

    Add MockRosBridge

--- tests/interfaces/test_robotinterface.py
+++ tests/interfaces/test_robotinterface.py
@@ -1,6 +1,8 @@
 from isar_turtlebot import Robot
 from robot_interface.test_robot_interface import interface_test
+from tests.mocks.ros_bridge import MockRosBridge
 
 
 def test_robotinterface():
-    interface_test(Robot())
+    robot = Robot(bridge=MockRosBridge())
+    interface_test(robot)
