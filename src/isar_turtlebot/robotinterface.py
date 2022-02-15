ebc79da4ae78f53ae931ae1d4d332cad20488d16
aestene <arnts@equinor.com>
2022-02-15 09:59:30 +0100

    Add transformation of coordinates
    Transformation of coordinates to and from asset frame was taken out of
    ISAR and moved to the robot specific repository.

--- src/isar_turtlebot/robotinterface.py
+++ src/isar_turtlebot/robotinterface.py
@@ -1,5 +1,9 @@
+import os
+from pathlib import Path
 from typing import Sequence
 
+from alitra import MapConfig, load_map_config
+from isar.services.coordinates.transformation import Transformation
 from robot_interface.models.inspection.inspection import Inspection
 from robot_interface.models.mission import InspectionTask, Task, TaskStatus
 from robot_interface.robot_interface import RobotInterface
@@ -10,7 +14,14 @@ from isar_turtlebot.turtlebot import Turtlebot
 
 class Robot(RobotInterface):
     def __init__(self, bridge: RosBridgeInterface = RosBridge):
-        self.turtlebot: Turtlebot = Turtlebot(bridge=bridge())
+        map_config: MapConfig = load_map_config(
+            Path(
+                os.path.dirname(os.path.realpath(__file__)),
+                "config/maps/turtleworld.json",
+            )
+        )
+        transform: Transformation = Transformation(map_config=map_config)
+        self.turtlebot: Turtlebot = Turtlebot(bridge=bridge(), transform=transform)
 
     def initiate_task(self, task: Task) -> bool:
         self.turtlebot.publish_task(task=task)
