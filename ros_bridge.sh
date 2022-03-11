2ddaab54ff4121325bf5e26ff028272c997986e5
Nicholas Dalhaug <nicd@equinor.com>
2022-03-11 12:26:25 +0100

    WIP: Messages are being bridged

--- ros_bridge.sh
+++ ros_bridge.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-source /opt/ros/melodic/setup.bash
-roscore > /dev/null 2>&1 &
+source /opt/ros/noetic/setup.bash
+#roscore > /dev/null 2>&1 &
 
-source /opt/ros/melodic/setup.bash
-# rosrun ros_ign_brigde parameter_bridge -h /world/car_world/pose/info > /dev/null 2>&1 &
+source /opt/ros/noetic/setup.bash
+# rosrun ros_ign_bridge parameter_bridge -h /world/car_world/pose/info > /dev/null 2>&1 &
 
 # ign launch -v 4 simulation.ign
