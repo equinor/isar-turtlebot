7d9d4f7b498b3da24c89ae8e99d970d6a948e628
aestene <arnts@equinor.com>
2022-08-19 15:01:20 +0200

    Run only roscore to debug radix pipeline

--- docker_scripts/entrypoint.sh
+++ docker_scripts/entrypoint.sh
@@ -3,37 +3,37 @@
 source /opt/ros/noetic/setup.bash
 source /home/catkin_ws/devel/setup.bash
 
-export TURTLEBOT3_MODEL=waffle
+# export TURTLEBOT3_MODEL=waffle
 
-WORLD_NAME="house"
-
-
+# WORLD_NAME="house"
 
 
-SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE}" )" >/dev/null && pwd )"
-CONFIG_FILE="$SCRIPTDIR/config/$WORLD_NAME.cfg"
+roscore
 
-source "$CONFIG_FILE"
-if [ ! $? -eq 0 ]
-then
-  printf "Unable to find config file $CONFIG_FILE\n"
-  exit 1
-fi
+# SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE}" )" >/dev/null && pwd )"
+# CONFIG_FILE="$SCRIPTDIR/config/$WORLD_NAME.cfg"
 
-if [ "$ENABLE_MANIPULATOR" = true ]
-then
-  if [ "$HEADLESS" = true ]
-  then
-    xvfb-run --auto-servernum --server-num=1 roslaunch isar_turtlebot turtlebot_manipulator.launch world_name:=$WORLD_NAME teleop_controller:=$TELEOP_CONTROLLER initial_pose_x:=$INITIAL_POSE_X initial_pose_y:=$INITIAL_POSE_Y x_pos:=$X_POS y_pos:=$Y_POS z_pos:=$Z_POS manipulator_gui:=$MANIPULATOR_GUI gui:=false headless:=true open_rviz:=false
-  else
-    roslaunch isar_turtlebot turtlebot_manipulator.launch world_name:=$WORLD_NAME teleop_controller:=$TELEOP_CONTROLLER initial_pose_x:=$INITIAL_POSE_X initial_pose_y:=$INITIAL_POSE_Y x_pos:=$X_POS y_pos:=$Y_POS z_pos:=$Z_POS manipulator_gui:=$MANIPULATOR_GUI
-  fi
+# source "$CONFIG_FILE"
+# if [ ! $? -eq 0 ]
+# then
+#   printf "Unable to find config file $CONFIG_FILE\n"
+#   exit 1
+# fi
 
-else
-  if [ "$HEADLESS" = true ]
-  then
-    xvfb-run --auto-servernum --server-num=1 roslaunch isar_turtlebot simulation.launch world_name:=$WORLD_NAME teleop_controller:=$TELEOP_CONTROLLER initial_pose_x:=$INITIAL_POSE_X initial_pose_y:=$INITIAL_POSE_Y x_pos:=$X_POS y_pos:=$Y_POS z_pos:=$Z_POS gui:=false headless:=true open_rviz:=false
-  else
-    roslaunch isar_turtlebot simulation.launch world_name:=$WORLD_NAME teleop_controller:=$TELEOP_CONTROLLER initial_pose_x:=$INITIAL_POSE_X initial_pose_y:=$INITIAL_POSE_Y x_pos:=$X_POS y_pos:=$Y_POS z_pos:=$Z_POS
-  fi
-fi
+# if [ "$ENABLE_MANIPULATOR" = true ]
+# then
+#   if [ "$HEADLESS" = true ]
+#   then
+#     xvfb-run --auto-servernum --server-num=1 roslaunch isar_turtlebot turtlebot_manipulator.launch world_name:=$WORLD_NAME teleop_controller:=$TELEOP_CONTROLLER initial_pose_x:=$INITIAL_POSE_X initial_pose_y:=$INITIAL_POSE_Y x_pos:=$X_POS y_pos:=$Y_POS z_pos:=$Z_POS manipulator_gui:=$MANIPULATOR_GUI gui:=false headless:=true open_rviz:=false
+#   else
+#     roslaunch isar_turtlebot turtlebot_manipulator.launch world_name:=$WORLD_NAME teleop_controller:=$TELEOP_CONTROLLER initial_pose_x:=$INITIAL_POSE_X initial_pose_y:=$INITIAL_POSE_Y x_pos:=$X_POS y_pos:=$Y_POS z_pos:=$Z_POS manipulator_gui:=$MANIPULATOR_GUI
+#   fi
+
+# else
+#   if [ "$HEADLESS" = true ]
+#   then
+#     xvfb-run --auto-servernum --server-num=1 roslaunch isar_turtlebot simulation.launch world_name:=$WORLD_NAME teleop_controller:=$TELEOP_CONTROLLER initial_pose_x:=$INITIAL_POSE_X initial_pose_y:=$INITIAL_POSE_Y x_pos:=$X_POS y_pos:=$Y_POS z_pos:=$Z_POS gui:=false headless:=true open_rviz:=false
+#   else
+#     roslaunch isar_turtlebot simulation.launch world_name:=$WORLD_NAME teleop_controller:=$TELEOP_CONTROLLER initial_pose_x:=$INITIAL_POSE_X initial_pose_y:=$INITIAL_POSE_Y x_pos:=$X_POS y_pos:=$Y_POS z_pos:=$Z_POS
+#   fi
+# fi
