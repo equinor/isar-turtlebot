56cc443e6af724c338870e0409a53b3a30048ad4
aestene <arnts@equinor.com>
2022-08-23 13:50:53 +0200

    Add additional verbose logging to docker build

--- docker_scripts/entrypoint.sh
+++ docker_scripts/entrypoint.sh
@@ -24,16 +24,16 @@ if [ "$ENABLE_MANIPULATOR" = true ]
 then
   if [ "$HEADLESS" = true ]
   then
-    xvfb-run --auto-servernum --server-num=1 roslaunch isar_turtlebot turtlebot_manipulator.launch world_name:=$WORLD_NAME teleop_controller:=$TELEOP_CONTROLLER initial_pose_x:=$INITIAL_POSE_X initial_pose_y:=$INITIAL_POSE_Y x_pos:=$X_POS y_pos:=$Y_POS z_pos:=$Z_POS manipulator_gui:=$MANIPULATOR_GUI gui:=false headless:=true open_rviz:=false
+    xvfb-run --auto-servernum --server-num=1 roslaunch isar_turtlebot turtlebot_manipulator.launch -v world_name:=$WORLD_NAME teleop_controller:=$TELEOP_CONTROLLER initial_pose_x:=$INITIAL_POSE_X initial_pose_y:=$INITIAL_POSE_Y x_pos:=$X_POS y_pos:=$Y_POS z_pos:=$Z_POS manipulator_gui:=$MANIPULATOR_GUI gui:=false headless:=true open_rviz:=false extra_gazebo_args:="--verbose"
   else
-    roslaunch isar_turtlebot turtlebot_manipulator.launch world_name:=$WORLD_NAME teleop_controller:=$TELEOP_CONTROLLER initial_pose_x:=$INITIAL_POSE_X initial_pose_y:=$INITIAL_POSE_Y x_pos:=$X_POS y_pos:=$Y_POS z_pos:=$Z_POS manipulator_gui:=$MANIPULATOR_GUI
+    roslaunch isar_turtlebot turtlebot_manipulator.launch -v world_name:=$WORLD_NAME teleop_controller:=$TELEOP_CONTROLLER initial_pose_x:=$INITIAL_POSE_X initial_pose_y:=$INITIAL_POSE_Y x_pos:=$X_POS y_pos:=$Y_POS z_pos:=$Z_POS manipulator_gui:=$MANIPULATOR_GUI extra_gazebo_args:="--verbose"
   fi
 
 else
   if [ "$HEADLESS" = true ]
   then
-    xvfb-run --auto-servernum --server-num=1 roslaunch isar_turtlebot simulation.launch world_name:=$WORLD_NAME teleop_controller:=$TELEOP_CONTROLLER initial_pose_x:=$INITIAL_POSE_X initial_pose_y:=$INITIAL_POSE_Y x_pos:=$X_POS y_pos:=$Y_POS z_pos:=$Z_POS gui:=false headless:=true open_rviz:=false
+    xvfb-run --auto-servernum --server-num=1 roslaunch isar_turtlebot simulation.launch -v world_name:=$WORLD_NAME teleop_controller:=$TELEOP_CONTROLLER initial_pose_x:=$INITIAL_POSE_X initial_pose_y:=$INITIAL_POSE_Y x_pos:=$X_POS y_pos:=$Y_POS z_pos:=$Z_POS gui:=false headless:=true open_rviz:=false extra_gazebo_args:="--verbose"
   else
-    roslaunch isar_turtlebot simulation.launch world_name:=$WORLD_NAME teleop_controller:=$TELEOP_CONTROLLER initial_pose_x:=$INITIAL_POSE_X initial_pose_y:=$INITIAL_POSE_Y x_pos:=$X_POS y_pos:=$Y_POS z_pos:=$Z_POS
+    roslaunch isar_turtlebot simulation.launch -v world_name:=$WORLD_NAME teleop_controller:=$TELEOP_CONTROLLER initial_pose_x:=$INITIAL_POSE_X initial_pose_y:=$INITIAL_POSE_Y x_pos:=$X_POS y_pos:=$Y_POS z_pos:=$Z_POS extra_gazebo_args:="--verbose"
   fi
 fi
